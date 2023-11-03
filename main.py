c59eb0c2a32b60e0de0bedd1f8047c5d29d30618
kseniia.tsch <ksenijatschuprina@gmail.com>
2023-11-04 00:33:52 +0200

    tasks 1 and 2 ready

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# # task 1 Даний текстовий файл. Необхідно створити новий файл, який потрібно переписати з першого файлу всі слова,
+# # що складаються не менше ніж з семи літер.
+# import re
+#
+# with open('input.txt', 'w') as file:
+#     file.write("In late summer, the hibiscus bloomed freely."
+#                "But as I left the house one day, something caught my eye."
+#                "In the middle of all those pristine white blooms was a solitary purple flower.")
+#
+# with open('input.txt', 'r') as file:
+#     text = file.read()
+#     word_pattern = r'\b[a-zA-Z-]{7,}\b'
+#     filtered_words = re.findall(word_pattern, text)
+#
+# get_words = [word for word in filtered_words if len(word) >= 7]
+#
+# with open('output.txt', 'w') as file:
+#     file.write("\n".join(get_words))
+#
+# task 2 Даний текстовий файл. Підрахувати кількість слів у ньому.
+#
+# user_input = input("Please enter a text: ")
+#
+# with open('user_input.txt', 'w') as file:
+#     file.write(user_input)
+#
+# with open('user_input.txt', 'r') as file:
+#     text = file.read()
+#     word_count = len(text.split())
+#
+# print(f"The number of words in the text is: {word_count}")
